7623d6f2003441363972d7f95ac3c2bc5230ab73
Ashima Taneja <ataneja34@gatech.edu>
2021-10-23 08:24:46 -0400

    sorting alg

--- parsePlaylists.py
+++ parsePlaylists.py
@@ -1,13 +1,17 @@
-import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy
+import json
+
 
 CLIENT_ID = "18e8f7f4ba1c467e9c77afbb34645234"
 CLIENT_SECRET = "be814f3e07234414a23a0c09eb231d07"
+client_credentials_manager = SpotifyClientCredentials(client_id= CLIENT_ID,
+                                                           client_secret=CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id= CLIENT_ID,
-                                                           client_secret=CLIENT_SECRET))
-
-#pl_id = 'spotify:playlist:3rMa2dmPtINY46bimugyrO'
+playlist_id = 'spotify:user:spotifycharts:playlist:37i9dQZEVXbJiZcmkrIHGU'
+results = sp.playlist(playlist_id)
+#print(json.dumps(results, indent=4))
 def getTracks(pl_ids): 
     offset = 0
     track_dict = {}
@@ -26,6 +30,14 @@ def getTracks(pl_ids):
             offset = offset + len(response['items'])
     return track_dict;
 
+def sortTracks(playlists):
+    tracks = getTracks(playlists)
+    sortedTracks = sorted(tracks.items(), key=lambda x: x[1], reverse=True)
+    return sortedTracks
 
-pl_ids = ["4EuoBAshVI1B0t5Rg6HTp7", "7DaNB3xvJ4F96C66mQgmpS", "7DaNB3xvJ4F96C66mQgmpS", "3rMa2dmPtINY46bimugyrO"]
-print(getTracks(pl_ids))
+playlists = ["4EuoBAshVI1B0t5Rg6HTp7", "7DaNB3xvJ4F96C66mQgmpS", "7DaNB3xvJ4F96C66mQgmpS", "3rMa2dmPtINY46bimugyrO"]
+
+sortedTracks = sortTracks(playlists)
+
+for i in sortedTracks:
+	print(i[0], i[1])
