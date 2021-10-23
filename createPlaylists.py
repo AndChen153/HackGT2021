ec7beb72857d8f455202bb4b80b24888c7c9d2cb
Andrew Chen <achen1192@gmail.com>
2021-10-23 03:26:05 -0400

    login

--- createPlaylists.py
+++ createPlaylists.py
@@ -2,7 +2,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 CLIENT_ID = "18e8f7f4ba1c467e9c77afbb34645234"
-CLIENT_SECRET = "4ed00f732cad40918222c053f68a5502"
+CLIENT_SECRET = "be814f3e07234414a23a0c09eb231d07"
 scope = "playlist-modify-public"
 SPOTIPY_REDIRECT_URI = 'http://localhost:8080'
 
@@ -10,5 +10,5 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope,
                      client_id = CLIENT_ID,
                      client_secret = CLIENT_SECRET,
                      redirect_uri = SPOTIPY_REDIRECT_URI))
-print(sp.user(sp.me()['id']))
+print(sp.user(sp.current_user))
 #sp.user_playlist_create(user = CLIENT_ID, name="playlist1", public = True, collaborative = False, description = "test playlist")
--- findPlaylists.py
+++ findPlaylists.py
@@ -2,7 +2,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 CLIENT_ID = "18e8f7f4ba1c467e9c77afbb34645234"
-CLIENT_SECRET = "4ed00f732cad40918222c053f68a5502"
+CLIENT_SECRET = "be814f3e07234414a23a0c09eb231d07"
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id= CLIENT_ID,
                                                            client_secret=CLIENT_SECRET))
