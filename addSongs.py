f3b9ac6f09a095c53624eec6100cf1767a6fdbb2
Andrew Chen <achen1192@gmail.com>
2021-10-23 10:44:06 -0400

    addsongs

--- addSongs.py
+++ addSongs.py
@@ -1,7 +1,5 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import json
-import requests
 import spotipy.util as util
 
 
@@ -17,15 +15,26 @@ sp = spotipy.Spotify(auth=token)
 
 songs = ["0Tfxvck4aXsRXffYakDzmF","6eT7xZZlB2mwyzJ2sUKG6w","0RRm4OS5ymfZryXBuj0G2m","2hloaUoRonYssMuqLCBLTX","3UrNOHCzVxX2KZbNcKQAyu"]
 
-ids = []
-names = []
-for i in range(len(sp.user_playlists(CLIENT_USERNAME)["items"])):
-    ids.append(sp.user_playlists(CLIENT_USERNAME)["items"][i]["id"])
-    names.append(sp.user_playlists(CLIENT_USERNAME)["items"][i]["name"])
+def findUserPlaylists():
+    ids = []
+    names = []
+    urls = []
+
+    for i in range(len(sp.user_playlists(CLIENT_USERNAME)["items"])):
+        ids.append(sp.user_playlists(CLIENT_USERNAME)["items"][i]["id"])
+        names.append(sp.user_playlists(CLIENT_USERNAME)["items"][i]["name"])
+        urls.append(sp.user_playlists(CLIENT_USERNAME)["items"][i]["external_urls"]["spotify"])
+        
+    return ids, names, urls
 
 def addSongs(songList, playlist_name):
+    ids, names, urls = findUserPlaylists()
     try:
         index = names.index(playlist_name)
-        sp.playlist_add_items
+        sp.playlist_add_items(ids[index], songList)
+
+        return("Songs added at " + urls[index])
     except:
         return("playlist not found")
+
+print(addSongs(songs, "randoPlaylist"))
--- createPlaylist.py
+++ createPlaylist.py
@@ -15,9 +15,25 @@ CLIENT_USERNAME = "18xwy2dfk7j0v61oyhj9b8csa"
 token = util.prompt_for_user_token(CLIENT_USERNAME,scope,client_id=CLIENT_ID,client_secret=CLIENT_SECRET,redirect_uri=SPOTIPY_REDIRECT_URI) 
 sp = spotipy.Spotify(auth=token)
 
+def findUserPlaylists():
+    ids = []
+    names = []
+    for i in range(len(sp.user_playlists(CLIENT_USERNAME)["items"])):
+        ids.append(sp.user_playlists(CLIENT_USERNAME)["items"][i]["id"])
+        names.append(sp.user_playlists(CLIENT_USERNAME)["items"][i]["name"])
+    return ids, names
+
+
 def createPlaylist(playlist_name):
+    ids, names = findUserPlaylists()
+
     try:
-        sp.user_playlist_create(CLIENT_USERNAME, name=playlist_name)
-        return(playlist_name + "was succesfully created as a playlist.")
+        if playlist_name in names:
+            return("Playlist name already exists.")
+        else:
+            sp.user_playlist_create(CLIENT_USERNAME, name=playlist_name)
+            return(playlist_name + " was succesfully created as a playlist.")
     except:
         return("Playlist could not be created, try again later.")
+
+print(createPlaylist("randoPlaylist1"))
