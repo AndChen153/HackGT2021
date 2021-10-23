76d1d7c6d5b38052b53e6003834a8c4e1f4864ab
Andrew Chen <achen1192@gmail.com>
2021-10-23 01:20:57 -0400

    test

--- test.py
+++ test.py
@@ -1,15 +1,9 @@
-# Shows artist info for a URN or URL
-
 import spotipy
-from spotipy import oauth2
+from spotipy.oauth2 import SpotifyClientCredentials
 
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="18e8f7f4ba1c467e9c77afbb34645234",
+                                                           client_secret="4ed00f732cad40918222c053f68a5502"))
 
-SPOTIPY_CLIENT_ID = '18e8f7f4ba1c467e9c77afbb34645234'
-SPOTIPY_CLIENT_SECRET = '4ed00f732cad40918222c053f68a5502'
-SCOPE = 'user-library-read'
-SPOTIPY_REDIRECT_URI = 'http://localhost:8080'
-
-
-sp_oauth = oauth2.SpotifyOAuth( SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI, scope=SCOPE)
-
-
+results = sp.search(q='weezer', limit=20)
+for idx, track in enumerate(results['tracks']['items']):
+    print(idx, track['name'])
